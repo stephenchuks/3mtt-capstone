0f16c1b4057c7a630e3682dd033c621871ece6cc
stephenchuks <stephenchuks@my.uopeople.edu>
2024-11-09 09:46:31 -0500

    Fix Database and port issues

--- 3mtt-capstone_project_fast/user-service/app/database.py
+++ 3mtt-capstone_project_fast/user-service/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://3mtt_capstone_user:Password@123@db:3306/3mtt_capstone"
+DATABASE_URL = "mysql+mysqlconnector://3mtt_capstone_user:Password%40123@db:3306/3mtt_capstone"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
